cd8bffc964bf781e6546572a33edb1c5a9b1baca
Sergey <ogresed@gmail.com>
2019-06-04 02:16:17 +0700

    Add address

---
+++ Task4.V3.Db/Migrations/20190603191524_WorkerAddAddress.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task4.V3.Migrations
+{
+    public partial class WorkerAddAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Workers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Workers");
+        }
+    }
+}
